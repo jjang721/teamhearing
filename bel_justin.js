e0c99d53b5de3e21d10171ea9433c40d64cbc78e
Justin Jang <144560172+jjang721@users.noreply.github.com>
2024-08-17 03:03:38 -0400

    Add files via upload

--- bel_justin.js
+++ bel_justin.js
@@ -7,72 +7,23 @@ function assignment(back) {
 	let a = 0, callbacks = [], options = [];
 
 	// Loudness Levels
-	options.push('Beginning Drum Test');
+	options.push('Beginning Sound Check');
 	callbacks.push(function(id){
 		protocol = new Protocol();
 		protocol.protocol = id;
 		gui.loudness(false,880);
 	}.bind(null,mode+'.'+a++));
-
-	// Detection Thresholds (110 to 3520 Hz)
-	options.push('Detection Thresholds (110 to 3520 Hz)');
-	callbacks.push(function(id){
-		protocol = new Protocol();
-		protocol.activity = 'percept';
-		protocol.callback = () => { assignment(); };
-		protocol.protocol = id;
-		protocol.random = false;
-		const f1 = [110,220,440,880,1760,3520];
-		for (let a = 0; a < f1.length; a++) {
-			protocol.settings.push({
-				alternatives: 3,
-				f1: f1[a]
-			})
-		}
-		protocol.start();
-	}.bind(null,mode+'.'+a++));
-
-	// Major/Minor Melodies
-	options.push('Melodies to Distinguish');
-	callbacks.push(function(id){
-		protocol = new Protocol();
-		protocol.activity = 'majorMinor';
-		protocol.callback = () => { assignment(); };
-		protocol.protocol = id;
-		protocol.settings.push({
-			practice: false,
-			stimulusMode: 0,
-			volume: true
-		});
-		protocol.start(3);
-	}.bind(null,mode+'.'+a++));
-
-
-	// Frequency Resolution (110 to 3520 Hz)
-	options.push('Frequency Discrimination (110 to 3520 Hz)');
-	callbacks.push(function(id){
-		protocol = new Protocol();
-		protocol.activity = 'percept';
-		protocol.callback = () => { assignment(); };
-		protocol.protocol = id;
-		const f1 = [110,220,440,880,1760,3520];
-		for (let a = 0; a < f1.length; a++) {
-			protocol.settings.push({
-				activity: 1,
-				f1: f1[a]
-			});
-		}
-		protocol.start(2);
-	}.bind(null,mode+'.'+a++));
-
+	
 	// Pitch Resolution for Piano Notes (A2, A3, A4)
-	options.push('Pitch Resolution for Piano Notes (A2, A3, A4)');
+	//first
+	options.push('Piano Notes High Pitch(A2, A3, A4)');
 	callbacks.push(function(id){
 		protocol = new Protocol();
 		protocol.activity = 'musescore';
 		protocol.callback = () => { assignment(); };
 		protocol.protocol = id;
-		const range = [[39,51],[51,63],[63,75]], f0 = [110,220,440];
+		// const range = [[39,51],[51,63],[63,75]], f0 = [110,220,440];
+		const range = [[79,83],[91,95],[103,107]], f0 = [110,220,440];
 		const instruments = ['piano'];
 		for (let a = 0; a < range.length; a++) {
 			protocol.settings.push({
@@ -85,6 +36,30 @@ function assignment(back) {
 		}
 		protocol.start(2);
 	}.bind(null,mode+'.'+a++));
+	
+	// Pitch Resolution for Piano Notes (A2, A3, A4)
+	//second
+	options.push('Perfect Fifth Trainer(A2, A3, A4)');
+	callbacks.push(function(id){
+		protocol = new Protocol();
+		protocol.activity = 'musescore';
+		protocol.callback = () => { assignment(); };
+		protocol.protocol = id;
+		// const range = [[39,51],[51,63],[63,75]], f0 = [110,220,440];
+		const range = [[48, 55], [60, 67], [72, 79]],f0 = [130.81, 261.63, 523.25];
+		const instruments = ['piano'];
+		for (let a = 0; a < range.length; a++) {
+			protocol.settings.push({
+				f0: f0[a],
+				instruments: instruments,
+				mode: 0,
+				range: range[a],
+				timbreMode: 0
+			});
+		}
+		protocol.start(2);
+	}.bind(null,mode+'.'+a++));
+	
 
 	// Melodic Contour
 	options.push('Melodic Contour Identification');
@@ -106,5 +81,5 @@ function assignment(back) {
 	}.bind(null,mode+'.'+a++));
 
 	// layout study
-	layout.study('Pitch Benchmark',options,callbacks,{},mode,back);
+	layout.study('Piano Music Tester',options,callbacks,{},mode,back);
 }
